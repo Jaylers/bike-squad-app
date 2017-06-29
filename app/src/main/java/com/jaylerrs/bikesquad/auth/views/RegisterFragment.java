92f79a84f615ccaef91d99d4ca1e773942ffd42e
Sapthawee Srichomthong <Mypandacm@hotmail.co.th>
2017-06-30 00:42:41 +0700

    remove unnecessary fields

--- app/src/main/java/com/jaylerrs/bikesquad/auth/views/RegisterFragment.java
+++ app/src/main/java/com/jaylerrs/bikesquad/auth/views/RegisterFragment.java
@@ -8,9 +8,11 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.jaylerrs.bikesquad.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -25,6 +27,9 @@ public class RegisterFragment extends Fragment {
     }
 
     View view;
+    @BindView(R.id.edt_reg_email) EditText reg_email;
+    @BindView(R.id.edt_reg_password) EditText reg_pass;
+    @BindView(R.id.edt_reg_confirm_password) EditText reg_con_pass;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,6 +41,12 @@ public class RegisterFragment extends Fragment {
         return view;
     }
 
+    @OnClick(R.id.btn_reg_submit) public void onRegister(){
+        if (isRegisterForm()){
+
+        }
+    }
+
     @OnClick(R.id.txt_register_back) public void onBack(){
         SignInFragment signInFragment = new SignInFragment();
         FragmentManager manager = getFragmentManager();
@@ -44,5 +55,23 @@ public class RegisterFragment extends Fragment {
                 R.anim.slide_down_out);
         ft.replace(R.id.frame_fragment_base_auth, signInFragment);
         ft.commit();
+    }
+
+    private Boolean isRegisterForm(){
+        if (reg_email.getText().toString().isEmpty()){
+            reg_email.setError(getActivity().getString(R.string.err_message_required));
+            return false;
+        }else if (reg_pass.getText().toString().isEmpty()){
+            reg_pass.setError(getActivity().getString(R.string.err_message_required));
+            return false;
+        }else if (reg_con_pass.getText().toString().isEmpty()){
+            reg_con_pass.setError(getActivity().getString(R.string.err_message_required));
+            return false;
+        }else if (!(reg_pass.getText().toString().equals(reg_con_pass.getText().toString()))){
+            reg_con_pass.setError(getActivity().getString(R.string.err_message_difference_password));
+            reg_pass.setText("");
+            reg_con_pass.setText("");
+            return false;
+        }else return true;
     }
 }
